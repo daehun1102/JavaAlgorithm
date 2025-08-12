8b922e1f18c0696ac2e0d354e8df130b2cf14c38
정대훈 <daehun1102@naver.com>
2025-08-12 17:56:47 +0900

    swea.by_category.permutation 정리

--- algorithm/src/boj/by_step/step04/pblm3052.java
+++ algorithm/src/boj/by_step/step04/pblm3052.java
@@ -3,8 +3,8 @@ package boj.by_step.step04;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 import java.util.StringTokenizer;
 
 /**
